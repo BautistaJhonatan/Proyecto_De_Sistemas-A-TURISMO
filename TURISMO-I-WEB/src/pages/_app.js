637076bd99d56bf3139898e356fb022397505d0b
iriarteAbiel <141873131+iriarteAbiel@users.noreply.github.com>
2023-11-24 01:21:27 -0400

    Proyecto completo web completo.
    Ultima actualizacion.

---
+++ TURISMO-I-WEB/src/pages/_app.js
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import 'leaflet/dist/leaflet.css';
+import '../styles/login.css';
+
+// import '../styles/locations.css';
+// import '../styles/add.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+
+
